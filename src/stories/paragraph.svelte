6219dca1dc8a1ba017a82690062dfa003a4c4df2
jakubfiglak <jakub.figlak@netguru.pl>
2023-08-26 19:52:14 +0200

    feat: install storybook, global stories

---
+++ src/stories/paragraph.svelte
@@ -0,0 +1,42 @@
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, ea possimus? Facilis corrupti et
+	consequuntur fugit ad. Iusto exercitationem debitis deleniti aliquam nihil cumque iure culpa. In
+	voluptate debitis aut, alias voluptatibus est fugiat provident rem, adipisci aspernatur magni
+	delectus eaque quod soluta. Accusamus, rem ad in excepturi nam assumenda numquam iste! Fuga
+	voluptas quidem consectetur, explicabo at quasi quam, dolorum corrupti ducimus omnis, quia vitae
+	fugit vero ipsum distinctio optio alias. Quod beatae ipsam, voluptate enim aliquam fuga sint
+	autem? Voluptatem eum est dolorem rem perspiciatis saepe eius adipisci, minus quasi facilis quam
+	consectetur aspernatur totam blanditiis, et id suscipit sapiente placeat? Optio dolores quo nisi
+	ipsum officia laudantium beatae esse enim consequatur in, dicta numquam magni necessitatibus alias
+	reiciendis eos. Mollitia reiciendis aperiam, nostrum fugiat quas vitae quisquam adipisci autem
+	blanditiis. Iusto in ea perferendis facilis iure corporis veniam repellat placeat quam quae
+	praesentium labore voluptatem dolores accusantium voluptate quibusdam velit voluptates error quasi
+	nesciunt unde doloribus, sunt tempora sit? Enim, dolor officiis! Beatae, blanditiis quibusdam.
+	Dolores iusto possimus quos! Velit alias quos deleniti. Deleniti fugiat alias, est iusto iure
+	voluptates voluptas. Enim iusto asperiores aliquid id fugit perferendis mollitia hic cupiditate
+	consequatur, quidem necessitatibus, repellat ut rerum. Repellendus, voluptate quis excepturi nulla
+	quo dolorum magnam eligendi labore id ducimus, saepe rem. Sit dolore voluptate et necessitatibus
+	doloremque incidunt ipsum sequi exercitationem. Ipsum ab totam deleniti. Temporibus nisi
+	reprehenderit eaque odio nostrum provident doloremque corrupti officia nam nobis aliquid facere
+	similique, dolorem, reiciendis aut amet quidem vero modi, non dignissimos assumenda! Iure
+	consectetur maiores, repellat corrupti eligendi soluta rem dolor ipsam aut odit dignissimos iste
+	dolorem et cumque nostrum ipsum modi reiciendis sunt laudantium aliquid quidem reprehenderit
+	blanditiis. Praesentium aperiam nisi itaque consequuntur odio! Eaque quas, delectus id minima illo
+	nesciunt alias aspernatur? Mollitia repellendus qui praesentium laboriosam iure eaque velit,
+	blanditiis sapiente aliquam veniam architecto excepturi cupiditate accusantium pariatur, animi
+	recusandae, fuga nesciunt accusamus voluptate aperiam similique. Eos neque facere omnis esse harum
+	ullam, doloribus cupiditate, ab officiis sit provident. Dolore est dicta pariatur illum, esse
+	corporis. Tempora sed laboriosam velit cumque omnis accusamus, facilis, totam provident odio
+	maiores a? Repellat dolor aliquid minima? Consequatur saepe similique sed quis voluptas voluptate
+	beatae molestiae ex quaerat. Vitae laboriosam dolore error corporis natus soluta tempora in
+	commodi dolorum! Sint obcaecati debitis excepturi, consectetur, quisquam quibusdam corporis animi
+	facilis esse natus ullam. Ea vitae alias nemo accusamus, ducimus dolores incidunt totam sequi
+	autem ad fuga reiciendis et non optio? Facere voluptatibus est sed ad? Eius, impedit dolores fuga
+	molestiae, accusantium debitis asperiores obcaecati velit tempore quaerat maxime, nobis optio!
+	Aperiam voluptas expedita rerum quidem eum impedit quae neque quod? Commodi iusto, autem atque
+	optio dolore dicta molestias dolorum modi aliquid iste ex aliquam rerum quidem ipsum consequatur,
+	sed doloremque, ratione quos? Repellendus, ducimus aliquam fugit nam quod quidem animi, error
+	placeat eaque voluptate architecto in quam autem cumque accusamus iste. Accusantium iure delectus
+	doloribus dignissimos voluptas id in a aut sit, consectetur repellendus ex eaque minima tempora
+	pariatur? Odio, enim.
+</p>
